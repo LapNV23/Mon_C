3c134027a02f6cbb5451ba02b147b55c6a9b75c2
LapNV23 <73330679+LapNV23@users.noreply.github.com>
2020-11-03 12:57:38 +0700

    Lab9

---
+++ Lab9_Arr/BT1_Lab9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+
+{
+	int arr[10], i;
+	printf("Nhap so:\n");
+	for(i = 1; i < 10; i++)
+    scanf("%d", &arr[i]);
+	
+	for(i = 1; i< 10; i+=2)
+	{
+		printf("\n%d", arr[i]);
+	}
+	
+}
