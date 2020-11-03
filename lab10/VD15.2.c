1111e73a0e3de0236978627518ac4683f2f687a8
LapNV23 <73330679+LapNV23@users.noreply.github.com>
2020-11-03 16:00:27 +0700

    Lab10

---
+++ lab10/VD15.2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ctr; /* ctr is global */
+void blk1(void);
+void blk2(void);
+void main(void);
+{
+	ctr = 10;
+	blk1();
+	.
+	.
+}
+void blk1(void)
+{
+	int rtc;
+	if(ctr > 8)
+	{
+		rtc = rtc + 1;
+		blk2();
+	}
+}
+void blk2(void)
+{
+	int ctr;
+	ctr = 0;
+}
---
+++ lab10/VD16.1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	printf("\nI am in main");
+	italy();
+	brazil();
+	argentinal();
+}
+italy()
+{
+	printf("\nI am italy");
+}
+brazil()
+{
+	printf("\nI am brazil");
+}
+argentinal()
+{
+	printf("\nI am argentinal");
+}
